7f1277018b852fefaf3c8e82343eba92d41c2509
Mario Picasso <mariopicasso@mi.unc.edu.ar>
2022-08-26 02:11:23 -0300

    def scommand and implement scommand_new function

---
+++ command.c
@@ -0,0 +1,25 @@
+#include <glib-2.0/glib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "command.h"
+
+struct scommand_s{
+    GList * command_and_args;
+    char * redirect_out;
+    char * redirect_in;
+};
+
+scommand scommand_new(void){
+    scommand result = malloc(sizeof(struct scommand_s));
+    result->command_and_args = NULL;
+    result->redirect_in = NULL;
+    result->redirect_out = NULL;
+
+    assert(result != NULL && scommand_is_empty(result) && 
+    scommand_get_redir_in (result) == NULL &&
+    scommand_get_redir_out (result) == NULL);
+    return result;    
+}
+
