00db8c7f076966857e4f56ab1c714fdb034a4a93
Mario Picasso <mariopicasso@mi.unc.edu.ar>
2022-08-26 18:54:30 -0300

    testing tad, scommand_is_empty, get_redir_inout

--- command.c
+++ command.c
@@ -1,6 +1,7 @@
 #include <glib-2.0/glib.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <assert.h>
 
 #include "command.h"
@@ -26,6 +27,11 @@ scommand scommand_new(void){
 scommand scommand_destroy(scommand self){
     assert(self != NULL);
     g_list_free(self->command_and_args);
+    self->command_and_args = NULL;
+    free(self->redirect_in);
+    self->redirect_in = NULL;
+    free(self->redirect_out);
+    self->redirect_out=NULL;
     free(self);
     self = NULL;
     assert(self == NULL);
@@ -34,10 +40,28 @@ scommand scommand_destroy(scommand self){
 
 void scommand_push_back(scommand self, char * argument){
     assert(self!=NULL && argument!=NULL);
-    g_list_append(self->command_and_args, argument); /*Esto anda si el argumento es un puntero de 64 bits y el gpointer también*/
+    GList *aux = g_list_append(self->command_and_args, argument); /*Esto anda si el argumento es un puntero de 64 bits y el gpointer también*/
+    self->command_and_args = aux;
     assert(!scommand_is_empty(self));
 }
 
 void scommand_pop_front(scommand self){
     assert(self!=NULL && !scommand_is_empty(self));
+}
+
+bool scommand_is_empty(const scommand self){
+    assert(self!=NULL);
+    return (self->command_and_args == NULL) && 
+    (self->redirect_in == NULL) &&
+    (self->redirect_out == NULL);
+}
+
+char * scommand_get_redir_in(const scommand self){
+    assert(self != NULL);
+    return self->redirect_in;
+}
+
+char * scommand_get_redir_out(const scommand self){
+    assert(self != NULL);
+    return self->redirect_out;
 }
